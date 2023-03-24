d291ed7b61421a62195bea69b769a9a0fb6ae2f2
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-03-24 22:43:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const s = new API('http://api.com/api/hello')
+console.log(s.isSecure()) 
 
+s.updateUrl('https://api.com/api/hello')
+console.log(s.isSecure()) 
+
+console.log(s.url) 
+
+console.log(s.method) 
+
+console.log(s.secure) 
 
 module.exports = { API }
