fdcf5dd6a01984c20a1bef78363dec7325231796
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-02 11:30:10 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,19 +1,1 @@
-class API {
-  #secure;
-  constructor(url, method = 'GET') {
-    this.url = url;
-    this.method = method;
-    this.#secure = url.startsWith('https');
-  }
 
-  isSecure() {
-    return this.#secure;
-  }
-
-  updateUrl(url) {
-    this.url = url;
-    this.#secure = url.startsWith('https');
-  }
-}
-const s = new API('http://api.com/api/hello');
-console.log(s.url);
