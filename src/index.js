2fdd4f68645376e3a28fef38aecbd2b53fdb4916
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-02 11:31:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 
-module.exports = { API }
 class API {
   #secure;
-
   constructor(url, method = 'GET') {
     this.url = url;
     this.method = method;
@@ -13,8 +11,20 @@ class API {
     return this.#secure;
   }
 
-  updateURL(url) {
+  updateUrl(url) {
     this.url = url;
     this.#secure = url.startsWith('https');
   }
 }
+
+const s = new API('http://api.com/api/hello')
+console. log(s.isSecure()) // false
+
+s.updateUrl('https://api. com/api/hello')
+console. log(s.isSecure()) // true
+
+console.log(s.url) // https://api. com/api/hello
+
+console.log(s.method) // GET
+
+console.log(s.secure) // undefined (because private field)
