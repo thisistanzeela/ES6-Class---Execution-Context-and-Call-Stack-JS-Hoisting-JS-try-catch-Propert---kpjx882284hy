d221ad4a894827ec04da2ee0a883168901834c9b
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-12 00:12:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,6 +27,26 @@ class API {
   }
 }
 
+
+// class API {
+//   #secure;
+//   constructor(url, method = 'GET') {
+//     this.url = url;
+//     this.method = method;
+//     this.#secure = url.startsWith('https');
+//   }
+
+//   isSecure() {
+//     return this.#secure;
+//   }
+
+//   updateUrl(url) {
+//     this.url = url;
+//     this.#secure = url.startsWith('https');
+//   }
+// }
+
+
 // const s = new API('http://api.com/api/hello')
 // console. log(s.isSecure()) // false
 
