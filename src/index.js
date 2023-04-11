7f65022163759e85b029cbe6a35179b9735465b0
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-11 23:32:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-
 class API {
   #secure;
+  
   constructor(url, method = 'GET') {
     this.url = url;
     this.method = method;
-    this.#secure = url.startsWith('https');
+    
+    if (url.startsWith('https')) {
+      this.#secure = true;
+    } else {
+      this.#secure = false;
+    }
   }
-
+  
   isSecure() {
     return this.#secure;
   }
-
+  
   updateUrl(url) {
     this.url = url;
-    this.#secure = url.startsWith('https');
+    
+    if (url.startsWith('https')) {
+      this.#secure = true;
+    } else {
+      this.#secure = false;
+    }
   }
 }
 
-const s = new API('http://api.com/api/hello')
-console. log(s.isSecure()) // false
+// const s = new API('http://api.com/api/hello')
+// console. log(s.isSecure()) // false
 
-s.updateUrl('https://api.com/api/hello')
-console. log(s.isSecure()) // true
+// s.updateUrl('https://api.com/api/hello')
+// console. log(s.isSecure()) // true
 
-console.log(s.url) // https://api. com/api/hello
+// console.log(s.url) // https://api. com/api/hello
 
-console.log(s.method) // GET
+// console.log(s.method) // GET
 
-console.log(s.secure) // undefined (because private field)
+// console.log(s.secure) // undefined (because private field)
