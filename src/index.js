42c928b29a65440d7cb7c16f9874b8158ef7b780
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-11 22:08:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ class API {
   }
 }
 
-const s = new API('http://api.com/api/hello')
-console. log(s.isSecure()) // false
+// const s = new API('http://api.com/api/hello')
+// console. log(s.isSecure()) // false
 
-s.updateUrl('https://api. com/api/hello')
-console. log(s.isSecure()) // true
+// s.updateUrl('https://api. com/api/hello')
+// console. log(s.isSecure()) // true
 
-console.log(s.url) // https://api. com/api/hello
+// console.log(s.url) // https://api. com/api/hello
 
-console.log(s.method) // GET
+// console.log(s.method) // GET
 
-console.log(s.secure) // undefined (because private field)
+// console.log(s.secure) // undefined (because private field)
