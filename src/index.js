3324f3804a358d70fa040a9c24c58206a3158ad1
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-24 20:28:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 
 
-// class API {
-//   #secure;
-//   constructor(url, method = 'GET') {
-//     this.url = url;
-//     this.method = method;
-//     this.#secure = url.startsWith('https');
-//   }
+class API {
+  #secure;
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
+  }
 
-//   isSecure() {
-//     return this.#secure;
-//   }
+  isSecure() {
+    return this.#secure;
+  }
 
-//   updateUrl(url) {
-//     this.url = url;
-//     this.#secure = url.startsWith('https');
-//   }
-// }
+  updateUrl(url) {
+    this.url = url;
+    this.#secure = url.startsWith('https');
+  }
+}
 
 
-// const s = new API('http://api.com/api/hello')
-// console. log(s.isSecure()) // false
+const s = new API('http://api.com/api/hello')
+console.log(s.isSecure()) // false
 
-// s.updateUrl('https://api.com/api/hello')
-// console. log(s.isSecure()) // true
+s.updateUrl('https://api.com/api/hello')
+console.log(s.isSecure()) // true
 
-// console.log(s.url) // https://api. com/api/hello
+console.log(s.url) // https://api. com/api/hello
 
-// console.log(s.method) // GET
+console.log(s.method) // GET
 
-// console.log(s.secure) // undefined (because private field)
+console.log(s.secure) // undefined (because private field)
